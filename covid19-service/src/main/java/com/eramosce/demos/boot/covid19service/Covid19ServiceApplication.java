4479d70864ac1cfe0058bef0bc7ca81df75a21ed
Eliezer Ramos <eramos@teamibr.com>
2020-05-21 21:14:24 -0400

    Freshly generated SpringBoot app from initializr

---
+++ covid19-service/src/main/java/com/eramosce/demos/boot/covid19service/Covid19ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eramosce.demos.boot.covid19service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19ServiceApplication.class, args);
+	}
+
+}
